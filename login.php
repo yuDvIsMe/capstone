1fdac0f3cdfe432d340d1cd402765b3659263ec8
12 <1@c.s>
2022-12-06 15:44:44 +0700

    update view password

--- login.php
+++ login.php
@@ -1,44 +1,40 @@
-<?php 
+<?php
 session_start();
 include("config.php");
-$error="";
-$msg="";
-if(isset($_REQUEST['login']))
-{
-	$email=$_REQUEST['email'];
-	$pass=$_REQUEST['pass'];
-	
-	$msg="";
+$error = "";
+$msg = "";
+if (isset($_REQUEST['login'])) {
+	$email = $_REQUEST['email'];
+	$pass = $_REQUEST['pass'];
 
-	if(isset($pass)) {
-	  $password = $pass;
-	  $number = preg_match('@[0-9]@', $password);
-	  $uppercase = preg_match('@[A-Z]@', $password);
-	  $lowercase = preg_match('@[a-z]@', $password);
-	  $specialChars = preg_match('@[^\w]@', $password);
-	 
-	  if(strlen($password) < 8 || !$number || !$uppercase || !$lowercase || !$specialChars) {
-		$msg = "<p class='alert alert-warning'>Mật khẩu phải có độ dài ít nhất 8 kí tự bao gồm số, chữ hoa, chữ thường và kí tự đặc biệt!";
-	  } else
-	  {
-		  if(!empty($email) && !empty($pass))
-	  {
-		  $sql = "SELECT * FROM user where uemail='$email' && upass='$pass'";
-		  $result=mysqli_query($con, $sql);
-		  $row=mysqli_fetch_array($result);
-			 if($row){
-				 
-				  $_SESSION['uid']=$row['uid'];
-				  $_SESSION['uemail']=$email;
-				  header("location:index.php");
-			 }
-			 else{
-				 $error = "<p class='alert alert-warning'>Email hoặc mật khẩu không chính xác.</p> ";
-			 }
-	  }else{
-		  $error = "<p class='alert alert-warning'>Vui lòng điền đẩy đủ thông tin</p>";
-	  }
-	  }
+	$msg = "";
+
+	if (isset($pass)) {
+		$password = $pass;
+		$number = preg_match('@[0-9]@', $password);
+		$uppercase = preg_match('@[A-Z]@', $password);
+		$lowercase = preg_match('@[a-z]@', $password);
+		$specialChars = preg_match('@[^\w]@', $password);
+
+		if (strlen($password) < 8 || !$number || !$uppercase || !$lowercase || !$specialChars) {
+			$msg = "<p class='alert alert-warning'>Mật khẩu phải có độ dài ít nhất 8 kí tự bao gồm số, chữ hoa, chữ thường và kí tự đặc biệt!";
+		} else {
+			if (!empty($email) && !empty($pass)) {
+				$sql = "SELECT * FROM user where uemail='$email' && upass='$pass'";
+				$result = mysqli_query($con, $sql);
+				$row = mysqli_fetch_array($result);
+				if ($row) {
+
+					$_SESSION['uid'] = $row['uid'];
+					$_SESSION['uemail'] = $email;
+					header("location:index.php");
+				} else {
+					$error = "<p class='alert alert-warning'>Email hoặc mật khẩu không chính xác.</p> ";
+				}
+			} else {
+				$error = "<p class='alert alert-warning'>Vui lòng điền đẩy đủ thông tin</p>";
+			}
+		}
 	}
 }
 ?>
@@ -46,130 +42,144 @@ if(isset($_REQUEST['login']))
 <html lang="en">
 
 <head>
-<!-- Required meta tags -->
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<!-- Meta Tags -->
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<link rel="shortcut icon" href="images/favicon.ico">
+	<!-- Meta Tags -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link rel="shortcut icon" href="images/favicon.ico">
 
-<!--	Fonts
+	<!--	Fonts
 	========================================================-->
-<link href="https://fonts.googleapis.com/css?family=Muli:400,400i,500,600,700&amp;display=swap" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Comfortaa:400,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Muli:400,400i,500,600,700&amp;display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Comfortaa:400,700" rel="stylesheet">
 
-<!--	Css Link
+	<!--	Css Link
 	========================================================-->
-<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-<link rel="stylesheet" type="text/css" href="css/bootstrap-slider.css">
-<link rel="stylesheet" type="text/css" href="css/jquery-ui.css">
-<link rel="stylesheet" type="text/css" href="css/layerslider.css">
-<link rel="stylesheet" type="text/css" href="css/color.css">
-<link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
-<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
-<link rel="stylesheet" type="text/css" href="fonts/flaticon/flaticon.css">
-<link rel="stylesheet" type="text/css" href="css/style.css">
-<link rel="stylesheet" type="text/css" href="css/login.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-slider.css">
+	<link rel="stylesheet" type="text/css" href="css/jquery-ui.css">
+	<link rel="stylesheet" type="text/css" href="css/layerslider.css">
+	<link rel="stylesheet" type="text/css" href="css/color.css">
+	<link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="fonts/flaticon/flaticon.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/login.css">
 
-<!--	Title
+	<!--	Title
 	=========================================================-->
-<title>Login to iHome - iHome | Giao dịch, cho thuê nhà đất uy tín hàng đầu</title>
+	<title>Login to iHome - iHome | Giao dịch, cho thuê nhà đất uy tín hàng đầu</title>
 </head>
+
 <body>
 
 
-<div id="page-wrapper">
-    <div class="row"> 
-        <!--	Header start  -->
-		<?php include("include/header.php");?>
-        <!--	Header end  -->
-        
-        <!--	Banner   --->
-        <div class="banner-full-row page-banner" style="background-image:url('images/breadcromb.jpg');">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-6">
-                        <h2 class="page-name float-left text-white text-uppercase mt-1 mb-0"><b>Đăng nhập</b></h2>
-                    </div>
-                </div>
-            </div>
-        </div>
-         <!--	Banner   --->
+	<div id="page-wrapper">
+		<div class="row">
+			<!--	Header start  -->
+			<?php include("include/header.php"); ?>
+			<!--	Header end  -->
 
-        <div class="page-wrappers login-body full-row bg-gray">
-            <div class="login-wrapper">
-            	<div class="container">
-                	<div class="loginbox">
-                        <div class="login-right">
-							<div class="login-right-wrap">
-								<h1>Đăng nhập vào iHome</h1>
-								<p class="account-subtitle">Xây những giá trị, dựng những ước mơ</p>
-								<?php echo $error; ?><?php echo $msg; ?>
-								<!-- Form -->
-								<form method="post">
-									<div class="form-group">
-										<input type="email"  name="email" class="form-control" required placeholder="Email">
+			<!--	Banner   --->
+			<div class="banner-full-row page-banner" style="background-image:url('images/breadcromb.jpg');">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-6">
+							<h2 class="page-name float-left text-white text-uppercase mt-1 mb-0"><b>Đăng nhập</b></h2>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!--	Banner   --->
+
+			<div class="page-wrappers login-body full-row bg-gray">
+				<div class="login-wrapper">
+					<div class="container">
+						<div class="loginbox">
+							<div class="login-right">
+								<div class="login-right-wrap">
+									<h1>Đăng nhập vào iHome</h1>
+									<p class="account-subtitle">Xây những giá trị, dựng những ước mơ</p>
+									<?php echo $error; ?><?php echo $msg; ?>
+									<!-- Form -->
+									<form method="post">
+										<div class="form-group">
+											<input type="email" name="email" class="form-control" required placeholder="Email">
+										</div>
+										<div class="form-group showpsw">
+											<input type="password" name="pass" class="form-control psw" placeholder="Mật khẩu">
+											<span class="show-btn"><i class="fas fa-eye"></i></span>
+										</div>
+										<p class="account-forgotpass"><a href="#" style="color: #000">Quên mật khẩu?</a></p>
+										<button class="btn btn-primary" name="login" value="Login" type="submit">Đăng nhập</button>
+									</form>
+									<div class="login-or">
+										<span class="or-line"></span>
+										<span class="span-or">or</span>
 									</div>
-									<div class="form-group">
-										<input type="password" name="pass"  class="form-control" required placeholder="Mật khẩu">
+
+									<!-- Social Login -->
+									<div class="social-login">
+										<span>Đăng nhập với</span>
+										<a href="#" class="facebook"><i class="fab fa-facebook-f"></i></a>
+										<a href="#" class="google"><i class="fab fa-google-plus-g"></i></a>
+										<a href="#" class="facebook"><i class="fab fa-twitter"></i></a>
 									</div>
-									<p class="account-forgotpass"><a href="#" style="color: #000">Quên mật khẩu?</a></p>
-									<button class="btn btn-primary" name="login" value="Login" type="submit">Đăng nhập</button>
-								</form>
-								<div class="login-or">
-									<span class="or-line"></span>
-									<span class="span-or">or</span>
+									<div class="text-center dont-have">Bạn chưa có tài khoản? <a href="register.php" style="color: #000">Đăng kí ngay!</a></div>
+
 								</div>
-								
-								<!-- Social Login -->
-								<div class="social-login">
-									<span>Đăng nhập với</span>
-									<a href="#" class="facebook"><i class="fab fa-facebook-f"></i></a>
-									<a href="#" class="google"><i class="fab fa-google-plus-g"></i></a>
-									<a href="#" class="facebook"><i class="fab fa-twitter"></i></a>
-								</div>
-								
-								
-								<div class="text-center dont-have">Bạn chưa có tài khoản? <a href="register.php" style="color: #000">Đăng kí ngay!</a></div>
-								
 							</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-	<!--	login  -->
-        
-        
-        <!--	Footer   start-->
-		<?php include("include/footer.php");?>
-		<!--	Footer   start-->
-        
-        <!-- Scroll to top --> 
-		<a href="#" class="bg-primary text-white hover-text-secondary" id="scroll"><i class="fas fa-angle-up"></i></a> 
-        <!-- End Scroll To top --> 
-    </div>
-</div>
-<!-- Wrapper End --> 
+						</div>
+					</div>
+				</div>
+			</div>
+			<!--	login  -->
 
-<!--	Js Link
-============================================================--> 
-<script src="js/jquery.min.js"></script> 
-<!--jQuery Layer Slider --> 
-<script src="js/greensock.js"></script> 
-<script src="js/layerslider.transitions.js"></script> 
-<script src="js/layerslider.kreaturamedia.jquery.js"></script> 
-<!--jQuery Layer Slider --> 
-<script src="js/popper.min.js"></script> 
-<script src="js/bootstrap.min.js"></script> 
-<script src="js/owl.carousel.min.js"></script> 
-<script src="js/tmpl.js"></script> 
-<script src="js/jquery.dependClass-0.1.js"></script> 
-<script src="js/draggable-0.1.js"></script> 
-<script src="js/jquery.slider.js"></script> 
-<script src="js/wow.js"></script> 
-<script src="js/custom.js"></script>
+
+			<!--	Footer   start-->
+			<?php include("include/footer.php"); ?>
+			<!--	Footer   start-->
+
+			<!-- Scroll to top -->
+			<a href="#" class="bg-primary text-white hover-text-secondary" id="scroll"><i class="fas fa-angle-up"></i></a>
+			<!-- End Scroll To top -->
+		</div>
+	</div>
+	<!-- Wrapper End -->
+
+	<!--	Js Link
+============================================================-->
+	<script src="js/jquery.min.js"></script>
+	<!--jQuery Layer Slider -->
+	<script src="js/greensock.js"></script>
+	<script src="js/layerslider.transitions.js"></script>
+	<script src="js/layerslider.kreaturamedia.jquery.js"></script>
+	<!--jQuery Layer Slider -->
+	<script src="js/popper.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/tmpl.js"></script>
+	<script src="js/jquery.dependClass-0.1.js"></script>
+	<script src="js/draggable-0.1.js"></script>
+	<script src="js/jquery.slider.js"></script>
+	<script src="js/wow.js"></script>
+	<script src="js/custom.js"></script>
+	<script>
+		const passField = document.querySelector(".psw");
+		const showBtn = document.querySelector("span i");
+		showBtn.onclick = (() => {
+			if (passField.type === "password") {
+				passField.type = "text";
+				showBtn.classList.add("hide-btn");
+			} else {
+				passField.type = "password";
+				showBtn.classList.remove("hide-btn");
+			}
+		});
+	</script>
 </body>
+
 </html>
